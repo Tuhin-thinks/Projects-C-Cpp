ff2f0976f2b09ad544a913be0102f89ffc929e47
tuhin-thinks <tuhinmitra190221@gmail.com>
2021-11-21 19:55:47 +0530

    updated source

--- Basic_IO_cpp/print_2f_precision.cpp
+++ Basic_IO_cpp/print_2f_precision.cpp
@@ -1,15 +1,35 @@
+#include <iterator>
 #include <iomanip>
 #include <iostream>
 
+float average_scores(float scores[], int n_scores){
+    float sum = 0.0, average = 0.0;
+    for(int i=0; i<n_scores; i++){
+        sum += scores[i];
+    }
+    return (sum / n_scores);
+}
+
+
 int main(){
 
-    float scores[9] = {85.004, 88.951, 89.5, 90, 75, 77.8, 85.6, 81.25, 89.69};
     
+    int n_scores;
+    std::cout << "Enter number of test scores:";
+    std::cin >> n_scores;
+    float scores[n_scores];
+    int count = 1;
     
-    for (float &score : scores)
-    {
-        std::cout << "Student A Test Scores: " << std::fixed << std::setprecision(2) << score << std::endl;
+    while (n_scores--){
+        std::cout << "Enter test score " << count << ":" << std::endl;
+        std::cin >> scores[count-1];
+        count ++;
     }
+    
+    // float scores[9] = {85.004, 88.951, 89.5, 90, 75, 77.8, 85.6, 81.25, 89.69};
+    float avg_score = average_scores(scores, count-1);
+    
+    std::cout << "The average of all test scores is: " << std::fixed << std::setprecision(2) << avg_score << std::endl;
     return 0;
     
 }

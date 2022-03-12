680c13dce7d6a500a785adb997d92d39c779e3fb
tuhin-thinks <tuhinmitra190221@gmail.com>
2022-03-12 15:58:16 +0530

    source updated 12-03-2022

--- Mathematical/README.md
+++ Mathematical/README.md
@@ -1,3 +1,4 @@
 ## Mathematical Problems
 
 1. [decimal to binary coversion](dec_to_bin.cpp)
+2. [To find power of a number](power_of_num.c)
---
+++ Mathematical/power_of_num.c
@@ -0,0 +1,12 @@
+#include <math.h>
+#include <stdio.h>
+
+int main(){
+    int a = 10;
+    int b = 2;
+
+    int power = pow(a, b);
+    printf("%d\n", power);
+
+    return 0;
+}
